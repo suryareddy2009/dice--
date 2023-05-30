890e3d1ec98a7b6cdbe08213d96fa908bcb25bd8
suryareddy2009 <106913625+suryareddy2009@users.noreply.github.com>
2023-05-30 19:07:46 +0530

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,66 @@
+import random
+
+response = input("Do you want to roll  (yes/no): ")
+
+
+if response or lol == "yes"  :
+    
+
+    
+
+    
+    
+        no= random.randint(1, 6)
+        if no == 1:
+
+        
+        
+            print("|   *   |")
+        
+        
+        elif no == 2:
+        
+            print("| *     |")
+      
+            print("|     * |")
+       
+        elif no == 3:
+       
+            print("| *     |")
+            print("|   *   |")
+            print("|     * |")
+      
+        elif no == 4:
+       
+            print("| *   * |")
+            print("|       |")
+            print("| *   * |")
+       
+        elif no == 5:
+       
+            print("| *   * |")
+            print("|   *   |")
+            print("| *   * |")
+       
+        else:
+        
+           print("| *   * |")
+           print("| *   * |")
+           print("| *   * |")
+
+
+           lol=input("do you want to roll the dice again  (yes/no):")
+
+
+
+
+    
+
+
+
+
+    
+        
+    
+    
+        
